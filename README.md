af044a0f96062d16b1a13f3c01175f0bc6c3da24
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-01-28 22:05:31 +0000

    Add Chakra UI

--- README.md
+++ README.md
@@ -3,11 +3,4 @@
 ## Tech Stack
 
 -   React with Vite
-
-## React Project Creation
-
-```bash
-npm create vite@latest .
-npm install
-npm run dev
-```
+-   Chakra UI
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ColorModeProvider } from '@/components/ui/color-mode';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
+// Render the app
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <ChakraProvider value={defaultSystem}>
+            <ColorModeProvider>
+                <App />
+            </ColorModeProvider>
+        </ChakraProvider>
+    </StrictMode>
+);
