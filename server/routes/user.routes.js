28c2f69f792c51cbbd13dca02fb9a66eed034792
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-11 12:04:01 +0000

    API: Follow / unfollow endpoint and protectRoute middleware

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signupUser, loginUser, logoutUser } from '../controllers/user.controller.js';
+import { signupUser, loginUser, logoutUser, followUnfollowUser } from '../controllers/user.controller.js';
+import protectRoute from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
+router.post('/follow/:id', protectRoute, followUnfollowUser);
 
 export default router;
